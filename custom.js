3adedac9c4cb206d3276a9ec9a216a56082582dc
justinkwork <37841550+justinkwork@users.noreply.github.com>
2018-06-01 09:55:20 -0500

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,40 @@
+function printActivities(viewModel) {
+	var a;
+	for (a = 0; a < viewModel.Activity.length; a++) {
+		if (viewModel.Activity[a].FullClassName === "Review Activity") {
+			window.open('/view/printView?workItemType=ReviewActivity&workItemId=' + viewModel.Activity[a].Id, '$_blank');
+		}
+		else {
+			window.open('/view/printView?workItemType=Activity&workItemId=' + viewModel.Activity[a].Id, '_blank');
+		}
+	}
+	
+}
+
+app.custom.formTasks.add('Incident', "Simple Print", function (formObj, viewModel) { 
+   window.open('/view/printView?workItemType=Incident&workItemId=' + viewModel.Id, '_blank');
+});
+
+app.custom.formTasks.add('Incident', "Simple Print Activities", function (formObj, viewModel) {
+	printActivities(viewModel);
+});
+
+app.custom.formTasks.add('ServiceRequest', "Simple Print", function (formObj, viewModel) { 
+   window.open('/view/printView?workItemType=ServiceRequest&workItemId=' + viewModel.Id, '_blank');
+});
+
+app.custom.formTasks.add('ServiceRequest', "Simple Print Activities", function (formObj, viewModel) {
+	printActivities(viewModel);
+});
+
+app.custom.formTasks.add('ChangeRequest', "Simple Print", function (formObj, viewModel) { 
+   window.open('/view/printView?workItemType=ChangeRequest&workItemId=' + viewModel.Id, '_blank');
+});
+
+app.custom.formTasks.add('ChangeRequest', "Simple Print Activities", function (formObj, viewModel) {
+	printActivities(viewModel);
+});
+
+app.custom.formTasks.add('Problem', "Simple Print", function (formObj, viewModel) { 
+   window.open('/view/printView?workItemType=Problem&workItemId=' + viewModel.Id, '_blank');
+});
---
+++ views/viewpanels/print-vp.js
@@ -0,0 +1,7 @@
+{
+    "Id": "print-vp",
+    "TypeId": "html",
+    "Definition": {
+        "content": "<h1 id='gridTitle'>Work Item</h1><div id='wiGrid'></div><h1 id='reviewerTitle'></h1><div id='reviewerGrid'></div><h1 id='logTitle'></h1><div id='logGrid'></div><h1 id='historyTitle'></h1><div id='historyGrid'></div><script src='/CustomSpace/formatPrint.js'></script> "
+    }
+}
